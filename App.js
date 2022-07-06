18ee2c90f023800ea832eae3ced26a85c7963b56
hhattimare7 <77083697+hhattimare7@users.noreply.github.com>
2022-07-06 06:05:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    //add the return satement for BottomTabNavigator- challenge 4
+    return <BottomTabNavigator/>;
+  }
+}
